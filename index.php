c76d9a050827fe39024d1940ce3e3060817cc75b
Igor Alves <igoralves1@hotmail.com>
2015-03-07 22:41:46 -0500

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html class="full" lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>InSilicoBr</title>
+    <link rel="icon" href="flags/shiny/ico/Brazil.ico">
+     <!-- Latest compiled and minified CSS -->
+       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+       <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <!-- Custom CSS -->
+    <link href="CSS/full.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header pull-left">     
+                <a class="navbar-brand" href="#"><em>In_Silico</em> <sup>{Br}</sup></a>
+            </div>
+            <div class="navbar-header pull-right">   
+                
+                <a class="navbar-brand" href="#"><i><img src="flags/shiny/24/Brazil.png"></i></a>
+                <a class="navbar-brand" href="https://github.com/igoralves1" target="_blank"><i class="fa fa-github-alt"></i> GitHub</a>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Put your page content here! -->
+ 
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                
+            </div>
+            <div class="col-md-6">
+                <div class="jumbotron top50">
+                    <h1><em>In_Silico</em> <sup>{Br}</sup></h1>
+                    <p class="lead">É um espaço dedicado à promoção do estudo da BioInformática entre os alunos brasileiros interessados no assunto.</p>
+                    <p><a class="btn btn-lg btn-warning" href="index_hp.php" role="button">Iniciar</a></p>
+                </div>
+
+                <footer class="rodape footer top17">
+                    <p>&COPY; Igor Alves 2015 <i class="fa fa-cog fa-spin"></i></p>
+                </footer>
+            </div>
+            <div class="col-md-3">
+                
+            </div>
+            
+        </div>
+  
+    
+    
+    
+    
+      </div>
+    
+    <!-- Put your page content here! -->
+    
+    
+    
+    
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
